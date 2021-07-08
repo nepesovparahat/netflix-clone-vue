1eaf5a9d06ca8a5f02b43351948017a1913c0ece
Parahat <nepesovparahatt@gmail.com>
2021-07-08 17:37:44 +0300

    Added axios and sass

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,13 +1,36 @@
 <template>
-  home
+  <div class="home">home
+    <h2>Netflix Orginals</h2>
+    <div class="home__posters" v-for="mov in movie.value" :key="mov.id">
+      <img :src="apiImgUrl+mov.poster_path" :alt="mov.name" style="height:300px" class="home_posters-img">
+    </div>
+  </div>
 </template>
 
 <script>
+import axios from 'axios';
+import { ref,onMounted, reactive } from 'vue';
 export default {
+setup(){
+  let movie = reactive({});
+  let apiImgUrl = ref("http://image.tmdb.org/t/p/w500");
+  const API = 'https://api.themoviedb.org/4/discover/tv?api_key=50c79094310c91407e637dc17af4ce13&sort_by=popularity.desc&page=1&with_networks=213';
+ async function fetchMovieData(){
+    const apiData = await axios.get(API)
+    .then((res) => res.data.results)
+    .catch((error) => console.log(error));
+    movie.value = apiData;
+    console.table(movie.value)
+  }
+onMounted(()=>{
+  fetchMovieData();
+})
+  return {fetchMovieData,movie,apiImgUrl}
+}
 
 }
 </script>
 
-<style>
+<style lang="scss">
 
 </style>
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [vue()],
+  alias: {
+    '@': path.resolve(__dirname, './src'),
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `@import '@/assets/scss/main.scss';`
+      }
+    }
+  }
 })
