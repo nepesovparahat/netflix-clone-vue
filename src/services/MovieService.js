31dd0af5a7497e61e89b3721016f248f2309ee44
Parahat <nepesovparahatt@gmail.com>
2021-07-15 19:11:41 +0300

    Updated movieService file

--- src/services/MovieService.js
+++ src/services/MovieService.js
@@ -1,13 +1,14 @@
-// import serviceInstance from "./tmdbAPI";
+import env from "@/env";
+const baseUrl = "https://api.themoviedb.org/3";
 
-// export default {
-//   getPopularMovies() {
-//     return serviceInstance.get(`/movie/popular?api_key=${API_KEY}`);
-//   },
-//   getTrendingMovies() {
-//     return serviceInstance.get(`/trending/movie/week?api_key=${API_KEY}`);
-//   },
-//   getTopMovies() {
-//     return serviceInstance.get(`/movie/top_rated?api_key=${API_KEY}`);
-//   }
-// };
+const requests = {
+  fetchTrending: `${baseUrl}/trending/all/week?api_key=${env.apiKey}`,
+  fetchOrginals: `${baseUrl}/discover/tv?api_key=${env.apiKey}&sort_by=popularity.desc&page=1&with_networks=213`,
+  fetchToprated: `${baseUrl}/movie/top_rated?api_key=${env.apiKey}`,
+  fetchActions: `${baseUrl}/discover/movie?api_key=${env.apiKey}&with_genres=28`,
+  fetchComedy: `${baseUrl}/discover/movie?api_key=${env.apiKey}&with_genres=35`,
+  fetchHorror: `${baseUrl}/discover/movie?api_key=${env.apiKey}&with_genres=27`,
+  fetchRomance: `${baseUrl}/discover/movie?api_key=${env.apiKey}&with_genres=10749`,
+  fetchDocumentaries: `${baseUrl}/discover/movie?api_key=${env.apiKey}&with_genres=99`,
+};
+export default requests;
